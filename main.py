fa5d32e9969ae146b88be0fe42062c5421620e21
sami10644 <61244442+sami10644@users.noreply.github.com>
2021-04-04 21:40:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def mathingWords(sentence1, sentence2):
 
 
 
+
 if __name__ == "__main__":
     # mathingWords("This is good", "python is good")
     sentences = ["python is a good", "this is snake",
